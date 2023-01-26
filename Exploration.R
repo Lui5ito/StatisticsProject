41b4abd2911cb5416169545b6e5607a922f5f194
Lui5ito <louis.allain@gmail.com>
2023-01-26 12:17:06 +0100

    Les données brutes et le premier fichier R ou on explore les données.

---
+++ Exploration.R
@@ -0,0 +1,37 @@
+library(Seurat)
+library(hdf5r)
+library(dplyr)
+library(ggplot2)
+data <- Read10X_h5("raw_feature_bc_matrix.D7.h5")
+
+gene_expression <- data$`Gene Expression`
+antibody_capture <- data$`Antibody Capture`
+
+summary(gene_expression)
+
+df <- as.data.frame(gene_expression[c(1:200), c(1:50)])
+
+sum_gene_df <- as.data.frame(colSums(gene_expression)) %>% 
+  rename("nb_transcrit" =`colSums(gene_expression)`) %>% 
+  filter(nb_transcrit != 0)
+
+ggplot(sum_gene_df, aes(x = nb_transcrit)) + 
+  geom_boxplot()
+
+
+boxplot(sum_gene_df)
+
+summary(sum_gene_df)
+apply(sum_gene_df)
+
+freq_table <- as.data.frame(ftable(as.data.frame(colSums(gene_expression))))
+
+
+
+plot(freq_table)
+
+ggplot(freq_table[26:nrow(freq_table),], aes(x = as.numeric(x), y = Freq)) + 
+  geom_point() +
+  scale_x_continuous(trans='log10') +
+  geom_jitter()
+

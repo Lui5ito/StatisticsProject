003e2908dc647a1993a3fb397fae772498167697
Lui5ito <louis.allain@gmail.com>
2023-04-17 12:10:03 +0200

    Test d'un nouvel algo de comparaison. Reste à comprendre comment marche le tableau 'resultats', à préparer le graph puis à le lancer sur le cluster.

--- Comparaison.R
+++ Comparaison.R
@@ -2,10 +2,7 @@ library(Seurat)
 library(hdf5r)
 library(dplyr)
 library(ggplot2)
-library(plotly)
-library(cowplot)
 library(DPQ)
-library(rlist)
 library(nloptr)
 
 #########################################################
@@ -13,20 +10,9 @@ library(nloptr)
 #########################################################
 
 rm(list=ls())
-set.seed(1664)
-#--# Pour créer l'échantillon #--#
-#####
-
-lambda_cible <- 3
-mu_cible <- 5050
-sigma_cible <- 1010
-data <- c(rpois(1000, lambda_cible), round(rnorm(500, mu_cible, sigma_cible)), round(rnorm(300, 2*mu_cible, sqrt(2)*sigma_cible)))
-
-
-
-
+set.seed(164)
 ################################################################################
-##-------------------------------FUNCTIONS------------------------------------##
+##-------------------------------FONCTIONS------------------------------------##
 ################################################################################
 
 logvraissemblance <- function(params){
@@ -53,21 +39,174 @@ logvraissemblance <- function(params){
 
 logvraissemblance_pour_nloptr <- function(params){return(-logvraissemblance(params))}
 
-algos <- c("NLOPT_LN_SBPLX", "NLOPT_LN_NELDERMEAD", "NLOPT_LN_COBYLA", "NLOPT_LN_BOBYQA")
 
 ################################################################################
 ##-------------------------------ALGORITHME-----------------------------------##
 ################################################################################
 
-results_algos <- list()
-erreurs_optim <- c()
+lambda_cible <- 3
+mu_cible <- 5050
+sigma_cible <- 1010
 
-iteration_SBPLX <- c()
-iteration_NELDERMEAD <- c()
-iteration_COBYLA <- c()
-iteration_BOBYQA <- c()
 
-nbre_donnes <- c(100, 500, 1000, 5000, 10000, 50000, 100000)
+taille_echantillon <- c(10^2)
+nbre_repetition <- 3
+algorithmes <- c("NLOPT_LN_NELDERMEAD", "NLOPT_LN_COBYLA", "NLOPT_LN_BOBYQA") #### Pas de "NLOPT_LN_SBPLX" car trop proche de L-BFGS-B et difficile de justifier qu'il ne marche pas.  
+nbre_random_start <- 6 #### 2x le nombre de paramètres.
+nbre_parametre_interet <- 9 #### log-vraisemblance complétée, temps, iterations, lambda, mu, sigma, pi1, pi2, pi3
+
+#!!# On ne s'intéresse ici que aux resultats finaux. On ne s'intéresse pas à l'évolution de la log-vraisemblance complétée ni à l'évolution de theta.
+#!!# En revanche, ces aspects sont fondamentals dans le rapport et dans l'exécution de l'algorithme sur nos données réelles.
+
+resultats <- array(data = NA, c(nbre_parametre_interet, length(taille_echantillon), nbre_repetition, length(algorithmes), nbre_random_start))
+# resultats[1:9,i,j,k,l]
+
+for (i in 1:length(taille_echantillon)){
+  print("taille_echantillon")
+  print(i)
+  taille_echantillon_en_cours <- taille_echantillon[i]
+  
+  for (j in 1:nbre_repetition){
+    print("nbre_repetition")
+    print(j)
+    data <- c(rpois(taille_echantillon_en_cours/2, lambda_cible), round(rnorm(taille_echantillon_en_cours/4, mu_cible, sigma_cible)), round(rnorm(taille_echantillon_en_cours/4, 2*mu_cible, sqrt(2)*sigma_cible)))
+    
+    for (k in 1:length(algorithmes)){
+      print(algorithmes[k])
+      print(k)
+      algo_en_cours <- algorithmes[k]
+      
+      for (l in 1:nbre_random_start){
+        print("nbre_random_start")
+        print(l)
+        
+        n <- length(data) ## Ici n vaut i mais j'ai déjà écrit n partout dans la suite...
+        
+        ## Initialisation aléatoire des paramètres
+        lambda_r <- data[sample(1:n, 1)]
+        mu_r <- data[sample(1:n, 1)]
+        sigma_r <- 100*sd(data)
+        pi1_r <- 1/3
+        pi2_r <- 1/3
+        pi3_r <- 1/3
+        Lvc <- c()
+        temps_nlopt <- c()
+        iteration_nlopt <- c()
+        
+        ## La boucle repeat EST l'algorithme EM
+        t <- try(repeat{
+          
+          ## On a bseoin de ces calculs pour lambda_r
+          logphi1 <- dpois(data, lambda_r, log = TRUE)
+          logphi2 <- logspace.sub(pnorm(data+1/2, mean = mu_r, sd = sigma_r, log.p = TRUE), pnorm(data-1/2, mean = mu_r, sd = sigma_r, log.p = TRUE))
+          logphi3 <- logspace.sub(pnorm(data+1/2, mean = 2*mu_r, sd = sqrt(2)*sigma_r, log.p = TRUE), pnorm(data-1/2, mean = 2*mu_r, sd = sqrt(2)*sigma_r, log.p = TRUE))
+          
+          ln1 <- log(pi1_r) + logphi1
+          ln2 <- log(pi2_r) + logphi2
+          ln3 <- log(pi3_r) + logphi3
+          
+          somme_phi_pondere <- pi1_r*exp(logphi1) + pi2_r*exp(logphi2) + pi3_r*exp(logphi3)
+          
+          t1 <- pi1_r*exp(logphi1) / somme_phi_pondere
+          t2 <- pi2_r*exp(logphi2) / somme_phi_pondere
+          t3 <- pi3_r*exp(logphi3) / somme_phi_pondere
+          
+          lv1 <- sum(t1*ln1)
+          lv2 <- sum(t2*ln2)
+          lv3 <- sum(t3*ln3)
+          
+          T1 <- sum(t1)
+          T2 <- sum(t2)
+          T3 <- sum(t3)
+          
+          ## On peut maintenant mettre à jour lambda_r
+          lambda_r <- sum(t1*data)/T1
+          
+          ## On doit calculer la log-vraisemblance à chaque itération puisque c'est notre porte de sortie de la boucle repeat
+          LV_r <- lv1+lv2+lv3
+          Lvc <- c(Lvc, LV_r)
+          
+          ## On optimise avec l'algorithme k
+          temps_ce_tour <- system.time(res_nlopt <- nloptr(x0 = c(mu_r, sigma_r), eval_f = logvraissemblance_pour_nloptr, opts = list(algorithm = algo_en_cours, maxeval = 10000, tol_rel=1e-15, xtol_abs=1e-15), lb = c(1, 1)))
+          temps_nlopt <- c(temps_nlopt, temps_ce_tour[3])
+          iteration_nlopt <- c(iteration_nlopt, res_nlopt$iteration)
+          
+          ## Mises à jours des paramètres
+          mu_r <- res_nlopt$solution[1]
+          sigma_r <- res_nlopt$solution[2]
+          pi1_r <- T1/n
+          pi2_r <- T2/n
+          pi3_r <- T3/n
+
+          
+          if (length(Lvc)>2 & abs((tail(Lvc, 1) - tail(Lvc, 2)[1])) < 0.1) { break }
+        })
+        
+        
+        ## On vérifie qu'il n'y a pas eu une seule erreur de nlopt dans l'algorithme EM
+        if(!(inherits(t, "try-error"))){
+          
+          ## On sauvegarde alors tous les paramètres d'intérêts finaux de notre algorithme EM
+          resultats[1, i, j, k, l] <- logvraissemblance(c(mu_r, sigma_r)) ## log-vraisemblance complétée
+          resultats[2, i, j, k, l] <- mean(temps_nlopt) ## On prend la moyenne de tous les nlopt utilisés lors de cet algorithme EM
+          resultats[3, i, j, k, l] <- mean(iteration_nlopt) ## On doit encore prendre la moyenne du nombre d'itération de nlopt lors de l'algorithme EM
+          resultats[4, i, j, k, l] <- lambda_r
+          resultats[5, i, j, k, l] <- mu_r
+          resultats[6, i, j, k, l] <- sigma_r
+          resultats[7, i, j, k, l] <- pi1_r
+          resultats[8, i, j, k, l] <- pi2_r
+          resultats[9, i, j, k, l] <- pi3_r
+          
+        }
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 for (k in nbre_donnes){
   data <- c(rpois(k/2, lambda_cible), round(rnorm(k/4, mu_cible, sigma_cible)), round(rnorm(k/4, 2*mu_cible, sqrt(2)*sigma_cible)))
